9cf5e670a9ea613589c16ab3f89f20c44543195d
Likhitha-302006 <vtu27724@veltech.edu.in>
2026-08-24 12:12:10 +0530

    Add files via upload

---
+++ Palindrome Index.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class Main {
+
+    public static int palindromeIndex(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            if (s.charAt(left) != s.charAt(right)) {
+
+                if (isPalindrome(s, left + 1, right)) {
+                    return left;
+                }
+
+                if (isPalindrome(s, left, right - 1)) {
+                    return right;
+                }
+
+                return -1;
+            }
+
+            left++;
+            right--;
+        }
+
+        return -1;
+    }
+
+    static boolean isPalindrome(String s, int left, int right) {
+
+        while (left < right) {
+
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String s = sc.next();
+            System.out.println(palindromeIndex(s));
+        }
+    }
+}
+
+Input
+2
+aaab
+baa
+
+Output
+3
+0
