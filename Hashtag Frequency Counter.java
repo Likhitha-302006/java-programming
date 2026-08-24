b57d063d0b87cad870ea8ad168016665ece9df14
Likhitha-302006 <vtu27724@veltech.edu.in>
2026-08-24 12:31:09 +0530

    Add files via upload

---
+++ Hashtag Frequency Counter.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Map<String, Integer> frequency = new LinkedHashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String hashtag = sc.next();
+
+            frequency.put(
+                hashtag,
+                frequency.getOrDefault(hashtag, 0) + 1
+            );
+        }
+
+        for (Map.Entry<String, Integer> entry : frequency.entrySet()) {
+            System.out.println(
+                entry.getKey() + " " + entry.getValue()
+            );
+        }
+
+        sc.close();
+    }
+}
+
+
+
+Input
+5
+java
+python
+java
+ai
+python
+
+Output
+java 2
+python 2
+ai 1
